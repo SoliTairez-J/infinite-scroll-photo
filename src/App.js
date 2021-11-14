c1196567fae8e5d2da70c744fe5b542436db9351
Warawit Siriyong <warawit.j@gmail.com>
2021-11-14 17:28:48 +0700

    finish commit

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import './App.css';
+import PhotoComponent from './components/PhotoComponent';
+import { useEffect, useState } from "react"
+function App() {
+  const apiKey = `rsVmVjSQXTdq-9eWq3vPbQmMFM0kcOlLDjHwq5QW-iQ`
+  const [photos,setPhotos] = useState([])
+  const [page,setPage] = useState(1)
+  const [isLoading,setIsLoading] = useState(false)
+
+  const fetchImage =async()=>{
+    setIsLoading(true)
+    try {
+      const apiUrl = `https://api.unsplash.com/photos/?client_id=${apiKey}&page=${page}`
+      const response = await fetch(apiUrl)
+      const data = await response.json()
+      setPhotos((oldData)=>{
+        return [...oldData,...data]
+      })
+    } catch (error) {
+      console.log(error);
+    }
+    setIsLoading(false)
+  }
+  useEffect(()=>{
+    fetchImage()
+    // eslint-disable-next-line
+  },[page])
+
+  useEffect(()=>{
+    const event = window.addEventListener('scroll',()=>{
+      if(window.innerHeight+window.scrollY>document.body.offsetHeight-500 && !isLoading){
+        setPage((oldPage)=>{
+          return oldPage+1
+        })
+      }
+    })
+    return ()=>window.removeEventListener('scroll',event)
+    // eslint-disable-next-line
+  },[])
+  return (
+    <main>
+      <h1>Infinite Scroll Photo | Unsplash API</h1>
+      <section className="photos">
+        <div className="display-photo">
+          {photos.map((data,index)=>{
+            return <PhotoComponent key={index} {...data}/>
+          })}
+        </div>
+      </section>
+    </main>
+  );
+}
+
+export default App;
